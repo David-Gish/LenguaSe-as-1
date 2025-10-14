50252e71653bf7364efbd09e7246a08702035282
Leyson David Asprilla Mosquera <145991327+Leyson-Gith@users.noreply.github.com>
2025-10-14 15:20:04 -0500

    Delete Belingues directory

--- Belingues/backend/db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2/promise')
-const mysqlPool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'dbprueba'
-})
-module.exports = mysqlPool
